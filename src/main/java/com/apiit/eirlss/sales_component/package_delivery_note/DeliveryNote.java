e5e245d987de1281080b7c3ad7a881f1f9958ee6
Sandaru <sandaru.sahan@gmail.com>
2020-10-18 18:09:27 +0530

    Customer Improvements

--- src/main/java/com/apiit/eirlss/sales_component/package_delivery_note/DeliveryNote.java
+++
@@ -1,8 +0,0 @@
-package com.apiit.eirlss.sales_component.package_delivery_note;
-
-import com.apiit.eirlss.sales_component.package_order.SalesOrder;
-
-public class DeliveryNote {
-    private int deliveryNoteId;
-    private SalesOrder salesOrder;
-}
